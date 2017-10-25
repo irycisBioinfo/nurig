d4afae0a8b2573da503bbd1ea31f8313bc5bf921
val <val@WorkStation>
2017-10-25 11:48:00 +0200

    update

--- lib/NuRIG.R
+++ lib/NuRIG.R
@@ -2,23 +2,37 @@ library(dplyr)
 library(tidyr)
 library(XML)
 
-#setwd("~/ST117/Paper/data/ST117/")
- #listFile = "list2"
- #annotFile = "annot.tab"
- #ref = "CP018065.1EnterococcusfaeciumstrainE1,completegenome.fasta"
- #ref = "GCA_001562905.1_ASM156290v1_genomic.fna"
- outFile = "out.xml"
+# setwd("~/ST117/Paper/data/ST117/")
+# listFile = "list"
+# annotFile = "annot.tab"
+# #ref = "CP018065.1EnterococcusfaeciumstrainE1,completegenome.fasta"
+# ref = "CP018069.1EnterococcusfaeciumstrainE1plasmidpE1_3,completesequence.fasta"
+# palette_color = "distinct"
 
-lista = read.table(listFile, sep = "\t")
-#colnames(lista) = c("file")
 
-if(exists("annotFile"))
-{
-  annot = read.delim(annotFile,sep = "\t", quote = "",stringsAsFactors = FALSE, header =FALSE)
+
+outFile = "out.xml"
+lista = read.delim(listFile, sep = "\t")
+
+
+if (exists("finalTable")) {
+  rm(finalTable)
 }
-if(!exists("palette_color"))
+
+
+if (exists("annotFile"))
 {
-	palette_color = 'rainbow'
+  annot = read.delim(
+    annotFile,
+    sep = "\t",
+    quote = "",
+    stringsAsFactors = FALSE,
+    header = FALSE
+  )
+}
+if (!exists("palette_color"))
+{
+  palette_color = 'rainbow'
 }
 
 if (ncol(lista) == 2)
@@ -29,64 +43,69 @@ if (ncol(lista) == 2)
   lista$names <- lista$file
 }
 
-print("Comparissons");
+print("Comparissons")
 
-j = 1
+system("rm out.coords")
+system("rm headers.txt")
+
+
 for (i in lista$file)
 {
-  system(paste("nucmer ", ref, i, "> /dev/null", collapse = ""))
-  system("delta-filter -q out.delta > out.f.delta ")
-  system("show-coords -l -L 200 -q -B out.f.delta > out.coords")
-  tmpTable = read.table("out.coords", sep = "\t")
-  colnames(tmpTable) = c(
-    "qID",
-    "date",
-    "lenq",
-    "alitype",
-    "referencefile",
-    "rID",
-    "startquery",
-    "endquery",
-    "startreference",
-    "endreference",
-    "peridentity",
-    "persimilarity",
-    "lenAlig",
-    "c16",
-    "c17",
-    "c18",
-    "c19",
-    "strand",
-    "lenref",
-    "c21",
-    "c22"
-  )
-  tmpTable$slot = j
-  tmpTable$file = i
-  
-  if (j > 1)
-  {
-    finalTable = bind_rows(finalTable, tmpTable)
-  } else{
-    finalTable = tmpTable
-  }
-  j = j + 1
-}
+  system(paste("nucmer", ref, i, "> /dev/null", collapse = ""))
+  system("delta-filter -r out.delta > out.f.delta ")
+  system("show-coords -l -r -B out.f.delta >> out.coords")
+  system(paste("grep -H '>' ",i," | sed 's/:>/\t/' | sed 's/ /\t/' | cut -f1,2 >> headers.txt", collapse=""))
 
-finalTable = full_join(finalTable, lista, by = "file")
+}
+tmpHeaders = read.table("headers.txt", sep = "\t", header = FALSE)
+colnames(tmpHeaders) = c("file", "qID")
+finalTable = read.delim2("out.coords",header = FALSE,  quote = "",  stringsAsFactors = FALSE,  sep = "\t")
+
+colnames(finalTable) = c(
+  "qID",
+  "date",
+  "lenq",
+  "alitype",
+  "referencefile",
+  "rID",
+  "startquery",
+  "endquery",
+  "startreference",
+  "endreference",
+  "peridentity",
+  "persimilarity",
+  "lenAlig",
+  "c16",
+  "c17",
+  "c18",
+  "c19",
+  "strand",
+  "lenref",
+  "c21",
+  "c22"
+)
+
+finalTable = left_join(finalTable,tmpHeaders)
+finalTable = left_join(finalTable, lista, by = "file")
+tmp = finalTable %>% select(file) %>% distinct()
+tmp$slot = 1:nrow(tmp)
+#theTable = finalTable %>% filter(!is.na(slot))
+finalTable = finalTable %>% left_join(tmp)
+finalTable = finalTable %>% mutate(peridentity = as.numeric(peridentity), persimilarity = as.numeric(persimilarity))
+
+
+
 
 theTable = finalTable
 
+finalTable = finalTable %>% filter(!is.na(slot)) %>%
+  group_by(rID) %>% summarise(ln = first(lenref)) %>% 
+  mutate(prueba = cumsum(ln)) %>% mutate(position = prueba -ln) %>%
+  select(rID, position) %>% full_join(finalTable, .)
 
+legend = finalTable %>% group_by(file, names) %>% 
+  summarise(score = sum(peridentity * lenAlig /100)) %>% arrange(desc(score))
 
-
-
-
-finalTable = theTable
-
-finalTable = finalTable %>% group_by(rID) %>% summarise(ln = first(lenref)) %>% mutate(prueba = cumsum(ln)) %>% mutate(position = prueba -ln) %>% select(rID, position) %>% full_join(finalTable, .)
-
-legend = finalTable %>% group_by(file,names) %>% summarise(score = sum(peridentity * lenAlig /100)) %>% arrange(desc(score))
 legend$order = seq(1:length(legend$file))
 
 finalTable = finalTable %>% full_join(legend)
@@ -119,10 +138,10 @@ if (palette_color == 'rainbow')
   colores = topo.colors(max(finalTable$slot))
 } else if (palette_color == 'terrain') {
   colores = terrain.colors(max(finalTable$slot))
-} else if (palette_color == 'distinct'){
-  colores = randomcoloR::distinctColorpalette_color(max(finalTable$slot))
+} else if (palette_color == 'distinct') {
+  colores = randomcoloR::distinctColorPalette(max(finalTable$slot))
 } else {
-  colores = colores = rainbow(max(finalTable$slot))
+  colores = rainbow(max(finalTable$slot))
 }
 
 for (tab in 1:max(legend$order))
@@ -232,14 +251,15 @@ if (nrow(contigs) > 1)
 
 ### Draw Annotation
 
-if(exists("annot"))
+if (exists("annot"))
 {
-  if(ncol(annot) <6){
+  if (ncol(annot) < 6) {
     annot$color = "black"
   }
-  colnames(annot) = c("rID","start","end","strand","label","color")
-  finalAnnot = finalTable %>% group_by(rID) %>% summarise(position = first(position)) %>% left_join(., annot, by ="rID") %>%  mutate(start = start +
-                                                                                                                            position, end = end + position)
+  colnames(annot) = c("rID", "start", "end", "strand", "label", "color")
+  finalAnnot = finalTable %>% group_by(rID) %>% 
+    summarise(position = first(position)) %>% left_join(., annot, by ="rID") %>%
+    mutate(start = start + position, end = end + position)
   
   xml$addTag("featureSlot",
              attrs = c(strand = "direct"),
@@ -251,17 +271,16 @@ if(exists("annot"))
   tmp = finalAnnot %>% filter(strand == "+")
   for (i in 1:(nrow(tmp)))
   {
-	  
-		  stop = tmp$end[i]
-		  start = tmp$start[i]
-	    if(stop > max(finalTable$lenref))
-	    {
-	      stop = max(finalTable$lenref)
-	    }
-		  if(start > max(finalTable$lenref))
-		  {
-		    start = max(finalTable$lenref)
-		  }
+    stop = tmp$end[i]
+    start = tmp$start[i]
+    if (stop > max(finalTable$lenref))
+    {
+      stop = max(finalTable$lenref)
+    }
+    if (start > max(finalTable$lenref))
+    {
+      start = max(finalTable$lenref)
+    }
     xml$addTag(
       "featureRange",
       attrs = c(
@@ -279,20 +298,21 @@ if(exists("annot"))
   xml$addTag("featureSlot",
              attrs = c(strand = "direct"),
              close = FALSE)
-  xml$addTag("feature",
-             attrs = c(decoration = "counterclockwise-arrow"),
-             close = FALSE)
+  xml$addTag(
+    "feature",
+    attrs = c(decoration = "counterclockwise-arrow"),
+    close = FALSE
+  )
   tmp = finalAnnot %>% filter(strand == "-")
   for (i in 1:(nrow(tmp)))
   {
-    
     stop = tmp$end[i]
     start = tmp$start[i]
-    if(stop > max(finalTable$lenref))
+    if (stop > max(finalTable$lenref))
     {
       stop = max(finalTable$lenref)
     }
-    if(start > max(finalTable$lenref))
+    if (start > max(finalTable$lenref))
     {
       start = max(finalTable$lenref)
     }
@@ -312,5 +332,4 @@ if(exists("annot"))
   xml$closeTag()
 }
 saveXML(xml$value(), file = "out.xml")
-#write.table(finalTable,file = "FinalTable.tsv",sep = '\t', row.names = FALSE)
 
